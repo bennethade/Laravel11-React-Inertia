dd57f2efe3a2fb90ed57f2678b4f1c09197d2253
bennethade <92984648+bennethade@users.noreply.github.com>
2025-06-10 14:58:21 +0100

    create new project started

---
+++ resources/js/Pages/Project/Create.jsx
@@ -0,0 +1,64 @@
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, useForm } from "@inertiajs/react";
+
+export default function Create(auth) {
+
+    const { data, setData, post, errors, reset } = useForm({
+        image: "",
+        name: "",
+        status: "",
+        description: "",
+        due_date: ""
+    })
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        post(route("project.create"));
+    }
+
+
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <div className="flex justify-between items-center">
+                    <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                        Create New Project
+                    </h2>
+
+                </div>
+
+            }
+        >
+
+            <Head title="Projects" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+                        <form onSubmit={onSubmit} className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                            <div>
+                                <InputLabel htmlFor="project_image_path" value="Project Image" />
+
+                                <TextInput
+                                    id="project_image_path"
+                                    type="file"
+                                    name="image"
+                                    value={data.image}
+                                    className="mt-1 block w-full"
+                                    onChange={e => setData('image', e.target.value)}
+                                />
+                            </div>
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </AuthenticatedLayout>
+    )
+}
